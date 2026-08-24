e6f2e869c9708c2ceec8736c08451472b74bb4f2
Gayathri Priya <priyagayathri2003@gmail.com>
2026-08-24 11:32:14 +0000

    Fix frontend movie APT URL

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,9 +6,10 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios.get('http://adac550308ffb46ab8812b872b68c452-427244242.us-east-1.elb.amazonaws.com/movies')
+      .then((response) => {
+        setMovies(response.data.movies);
+      });
   }, []);
 
   return (
