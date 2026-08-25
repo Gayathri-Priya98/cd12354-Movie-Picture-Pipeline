712b0955ffbd20be6c2b847ec2eca60c4a6632e8
Gayathri Priya <priyagayathri2003@gmail.com>
2026-08-25 09:56:39 +0000

    Fix frontend formatting

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -14,7 +14,11 @@ function MovieList({ onMovieClick }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li 
+          className="movieItem" 
+          key={movie.id} 
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
